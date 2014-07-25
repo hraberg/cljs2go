e6bc862360e900c92e6a91cc50140cd42b9ee80d
Håkan Råberg <hraberg@yahoo.se>
2014-07-25 20:22:56 +0100

    More realistic names

--- main.go
+++ main.go
@@ -5,50 +5,50 @@ import (
 	"reflect"
 )
 
-type Bar struct{}
+type Foo struct{}
 
-func (b Bar) Bar_1(x interface{}) interface{} {
+func (f Foo) Bar_1(x interface{}) interface{} {
 	fmt.Printf("%d\n", x)
-	return "Bar1"
+	return "Bar_1"
 }
 
-func (b Bar) Bar_2(x interface{}, y interface{}) interface{} {
+func (f Foo) Bar_2(x interface{}, y interface{}) interface{} {
 	fmt.Printf("%d %d\n", x, y)
-	return "Bar2"
+	return "Bar_2"
 }
 
-func (b Bar) Bar_2_VA(x, y interface{}, xs ...interface{}) interface{} {
+func (f Foo) Bar_2_VA(x, y interface{}, xs ...interface{}) interface{} {
 	fmt.Printf("%d %d %d\n", x, y, xs)
-	return "BarVA"
+	return "Bar_2_VA"
 }
 
-func (b Bar) ApplyTo(xs []interface{}) interface{} {
+func (f Foo) ApplyTo(xs []interface{}) interface{} {
 	var l = len(xs)
 	switch {
 	case l > 2:
-		var v = reflect.ValueOf(b)
+		var v = reflect.ValueOf(f)
 		var vs = make([]reflect.Value, len(xs))
 		for i, x := range xs {
 			vs[i] = reflect.ValueOf(x)
 		}
 		return v.MethodByName("Bar_2_VA").Call(vs)[0].Interface()
 	case l == 1:
-		return b.Bar_1(xs[0])
+		return f.Bar_1(xs[0])
 	case l == 2:
-		return b.Bar_2(xs[0], xs[1])
+		return f.Bar_2(xs[0], xs[1])
 	}
 	panic(fmt.Sprintf("Invalid arity: %d", l))
 }
 
-func (b Bar) Bar(xs ...interface{}) interface{} {
+func (f Foo) Bar(xs ...interface{}) interface{} {
 	var l = len(xs)
 	switch {
 	case l > 2:
-		return b.ApplyTo(xs)
+		return f.ApplyTo(xs)
 	case l == 1:
-		return b.Bar_1(xs[0])
+		return f.Bar_1(xs[0])
 	case l == 2:
-		return b.Bar_2(xs[0], xs[1])
+		return f.Bar_2(xs[0], xs[1])
 	}
 	panic(fmt.Sprintf("Invalid arity: %d", l))
 }
@@ -82,35 +82,35 @@ func plus_one(x interface{}) interface{} {
 func main() {
 	fmt.Printf("ClojureScript to Go [go] %v\n", plus_one(1))
 
-	var bar = Bar{}
-	fmt.Printf("%v\n", bar.Bar(8))
+	var foo = Foo{}
+	fmt.Printf("%v\n", foo.Bar(8))
 
 	var xs = []int{4, 5, 6, 7}
 	var is = make([]interface{}, len(xs))
 	for i, x := range xs {
 		is[i] = x
 	}
-	fmt.Printf("%v\n", bar.ApplyTo(is))
+	fmt.Printf("%v\n", foo.ApplyTo(is))
 	xs = []int{4}
 	is = make([]interface{}, len(xs))
 	for i, x := range xs {
 		is[i] = x
 	}
-	fmt.Printf("%v\n", bar.ApplyTo(is))
+	fmt.Printf("%v\n", foo.ApplyTo(is))
 
 	xs = []int{8, 9}
 	is = make([]interface{}, len(xs))
 	for i, x := range xs {
 		is[i] = x
 	}
-	fmt.Printf("%v\n", bar.ApplyTo(is))
+	fmt.Printf("%v\n", foo.ApplyTo(is))
 
-	fmt.Printf("%v\n", bar.Bar_1(2))
-	fmt.Printf("%v\n", bar.Bar_2(2, 3))
-	fmt.Printf("%v\n", bar.Bar_2_VA(2, 3, 4))
+	fmt.Printf("%v\n", foo.Bar_1(2))
+	fmt.Printf("%v\n", foo.Bar_2(2, 3))
+	fmt.Printf("%v\n", foo.Bar_2_VA(2, 3, 4))
 
-	fmt.Printf("%v\n", bar.Bar(2))
-	fmt.Printf("%v\n", bar.Bar(2, 3))
-	fmt.Printf("%v\n", bar.Bar(2, 3, 4))
-	fmt.Printf("%v\n", bar.Bar())
+	fmt.Printf("%v\n", foo.Bar(2))
+	fmt.Printf("%v\n", foo.Bar(2, 3))
+	fmt.Printf("%v\n", foo.Bar(2, 3, 4))
+	fmt.Printf("%v\n", foo.Bar())
 }
