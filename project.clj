88f97781060bc9e4841e91b04551da507b6bd0a6
Håkan Råberg <hraberg@yahoo.se>
2014-08-10 21:58:59 +0100

    core.async 0.1.319.0-6b1aca-alpha

--- project.clj
+++ project.clj
@@ -5,4 +5,5 @@
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :main cljs.go
   :dependencies [[org.clojure/clojure "1.7.0-alpha1"]
-                 [org.clojure/clojurescript "0.0-2311"]])
+                 [org.clojure/clojurescript "0.0-2311"]
+                 [org.clojure/core.async "0.1.319.0-6b1aca-alpha"]])
