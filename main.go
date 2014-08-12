cc12c60b4c6c78fa0f13c4e6abe459ca68eb267f
Håkan Råberg <hraberg@yahoo.se>
2014-08-12 10:06:31 +0100

    Simplifying, lets cross the protocol bridge when we reach it.

--- main.go
+++ main.go
@@ -1,9 +1,6 @@
 package main
 
-import (
-	"fmt"
-	"reflect"
-)
+import "fmt"
 
 /*
 ;; IFn
@@ -33,58 +30,38 @@ import (
 
 */
 
-type Foo struct{}
-
-type Bar interface {
-	Bar(...interface{}) interface{}
-}
-
 var Bar_cljs__lang__maxFixedArity = 2
 
-func (f Foo) Bar_cljs__core__IFn___invoke__arity__1(x interface{}) interface{} {
+func Bar_cljs__core__IFn___invoke__arity__1(x interface{}) interface{} {
 	fmt.Printf("%d\n", x)
 	return "Bar_cljs__core__IFn___invoke__arity__1"
 }
 
-func (f Foo) Bar_cljs__core__IFn___invoke__arity__2(x interface{}, y interface{}) interface{} {
+func Bar_cljs__core__IFn___invoke__arity__2(x interface{}, y interface{}) interface{} {
 	fmt.Printf("%d %d\n", x, y)
 	return "Bar_cljs__core__IFn___invoke__arity__2"
 }
 
-func (f Foo) Bar_cljs__core__IFn___invoke__arity__variadic(x, y interface{}, xs ...interface{}) interface{} {
+func Bar_cljs__core__IFn___invoke__arity__variadic(x, y interface{}, xs ...interface{}) interface{} {
 	fmt.Printf("%d %d %d\n", x, y, xs)
 	return "Bar_cljs__core__IFn___invoke__arity__variadic"
 }
 
-func (f Foo) Bar_cljs__lang__applyTo(xs []interface{}) interface{} {
+func Bar(xs ...interface{}) interface{} {
 	var l = len(xs)
 	switch {
-	case l > Bar_cljs__lang__maxFixedArity:
-		var v = reflect.ValueOf(f)
-		var vs = make([]reflect.Value, len(xs))
-		for i, x := range xs {
-			vs[i] = reflect.ValueOf(x)
-		}
-		return v.MethodByName("Bar_cljs__core__IFn___invoke__arity__variadic").Call(vs)[0].Interface()
+	case l > 2:
+		return Bar_cljs__core__IFn___invoke__arity__variadic(xs[0], xs[1], xs[2:]...)
 	case l == 1:
-		return f.Bar_cljs__core__IFn___invoke__arity__1(xs[0])
+		return Bar_cljs__core__IFn___invoke__arity__1(xs[0])
 	case l == 2:
-		return f.Bar_cljs__core__IFn___invoke__arity__2(xs[0], xs[1])
+		return Bar_cljs__core__IFn___invoke__arity__2(xs[0], xs[1])
 	}
 	panic(fmt.Sprintf("Invalid arity: %d", l))
 }
 
-func (f Foo) Bar(xs ...interface{}) interface{} {
-	var l = len(xs)
-	switch {
-	case l > Bar_cljs__lang__maxFixedArity:
-		return f.Bar_cljs__lang__applyTo(xs)
-	case l == 1:
-		return f.Bar_cljs__core__IFn___invoke__arity__1(xs[0])
-	case l == 2:
-		return f.Bar_cljs__core__IFn___invoke__arity__2(xs[0], xs[1])
-	}
-	panic(fmt.Sprintf("Invalid arity: %d", l))
+func Bar_cljs__lang__applyTo(xs []interface{}) interface{} {
+	return Bar(xs...)
 }
 
 func double(x interface{}) float64 {
@@ -116,35 +93,32 @@ func plus_one(x interface{}) interface{} {
 func main() {
 	fmt.Printf("ClojureScript to Go [go] %v\n", plus_one(1))
 
-	var foo = Foo{}
-	fmt.Printf("%v\n", foo.Bar(8))
-
 	var xs = []int{4, 5, 6, 7}
 	var is = make([]interface{}, len(xs))
 	for i, x := range xs {
 		is[i] = x
 	}
-	fmt.Printf("%v\n", foo.Bar_cljs__lang__applyTo(is))
+	fmt.Printf("%v\n", Bar_cljs__lang__applyTo(is))
 	xs = []int{4}
 	is = make([]interface{}, len(xs))
 	for i, x := range xs {
 		is[i] = x
 	}
-	fmt.Printf("%v\n", foo.Bar_cljs__lang__applyTo(is))
+	fmt.Printf("%v\n", Bar_cljs__lang__applyTo(is))
 
 	xs = []int{8, 9}
 	is = make([]interface{}, len(xs))
 	for i, x := range xs {
 		is[i] = x
 	}
-	fmt.Printf("%v\n", foo.Bar_cljs__lang__applyTo(is))
+	fmt.Printf("%v\n", Bar_cljs__lang__applyTo(is))
 
-	fmt.Printf("%v\n", foo.Bar_cljs__core__IFn___invoke__arity__1(2))
-	fmt.Printf("%v\n", foo.Bar_cljs__core__IFn___invoke__arity__2(2, 3))
-	fmt.Printf("%v\n", foo.Bar_cljs__core__IFn___invoke__arity__variadic(2, 3, 4))
+	fmt.Printf("%v\n", Bar_cljs__core__IFn___invoke__arity__1(2))
+	fmt.Printf("%v\n", Bar_cljs__core__IFn___invoke__arity__2(2, 3))
+	fmt.Printf("%v\n", Bar_cljs__core__IFn___invoke__arity__variadic(2, 3, 4))
 
-	fmt.Printf("%v\n", foo.Bar(2))
-	fmt.Printf("%v\n", foo.Bar(2, 3))
-	fmt.Printf("%v\n", foo.Bar(2, 3, 4))
-	fmt.Printf("%v\n", foo.Bar())
+	fmt.Printf("%v\n", Bar(2))
+	fmt.Printf("%v\n", Bar(2, 3))
+	fmt.Printf("%v\n", Bar(2, 3, 4))
+	fmt.Printf("%v\n", Bar())
 }
