47075fc617170fc95b7c90bece923218570aa895
Håkan Råberg <hraberg@yahoo.se>
2014-08-13 14:43:37 +0100

    We're likley not to know the type until we actually call it.

--- cljs/core/core.go
+++ cljs/core/core.go
@@ -4,15 +4,16 @@ import "github.com/hraberg/cljs.go/js"
 
 // This file will eventually be generated from cljs.core.
 
-var _STAR_print_fn_STAR_ = func(objs ...interface{}) interface{} {
+var _STAR_print_fn_STAR_ interface{} = func(objs ...interface{}) interface{} {
 	panic(js.Error{"No *print-fn* fn set for evaluation environment"})
 }
 
 func Println(objs ...interface{}) interface{} {
-	_STAR_print_fn_STAR_(objs...)
+	_STAR_print_fn_STAR_.(func(...interface{}) interface{})(objs...)
 	return nil
 }
 
-func Set_print_fn_BANG_(f func(objs ...interface{}) interface{}) {
+func Set_print_fn_BANG_(f interface{}) interface{} {
 	_STAR_print_fn_STAR_ = f
+	return nil
 }
