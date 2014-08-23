8a828c6a9e03cbbc197abc6015b0ea30f2a8e9ef
Håkan Råberg <hraberg@yahoo.se>
2014-08-23 21:09:25 +0100

    Test cleanuo

--- test/cljs/go_test.clj
+++ test/cljs/go_test.clj
@@ -2,17 +2,13 @@
   (:refer-clojure :exclude [test])
   (:require [clojure.test :refer :all]
             [cljs.go :refer :all]
-            [cljs.tagged-literals :refer [*cljs-data-readers*]]
+            [cljs.tagged-literals ]
+            [clojure.pprint :as pp]
             [clojure.string :as s]
             [clojure.java.io :as io]
             [clojure.java.shell :as sh])
-  (:import [java.io File Writer]
+  (:import [java.io Writer]
            [cljs.tagged_literals JSValue]))
-
-(defmethod print-method cljs.tagged_literals.JSValue
-  [^JSValue d ^Writer w]
-  (.write w "#js ")
-  (print-method (.val d) w))
 
 (defn go-test [package]
   (let [{:keys [out err exit]} (sh/sh "go" "test" (str package))
@@ -26,29 +22,24 @@
 
 (defn test-comment [code ast]
   (println "/*")
-  (clojure.pprint/pprint code)
+  (pp/pprint code)
   (println)
-  (clojure.pprint/pprint ast)
+  (pp/pprint ast)
   (println"*/"))
 
 (defn test-setup [setup]
-  (let [ast (cljs->ast setup)]
-    (test-comment setup ast)
-    (printf "\t%s\n" (ast->go ast))))
-
-(defn test-setup-toplevel [setup]
   (with-out-str
-    (test-setup setup)))
+    (let [ast (cljs->ast setup)]
+      (test-comment setup ast)
+      (printf "\t%s\n" (ast->go ast)))))
 
 (defn test [test & assertions]
   (with-out-str
     (printf "func Test_%s(t *testing.T) {\n" (name test))
-    (doseq [[expected actual] assertions
+    (doseq [[expected actual] (partition 2 assertions)
             :let [ast (-> [actual] (cljs->ast (assoc (cljs.analyzer/empty-env) :context :expr)))]]
       (test-comment actual (first ast))
-      (printf "\tassert.Equal(t%s, %s)\n"
-              (if (nil? expected) "" (str ", \n" expected))
-              (str "\n" (ast->go ast))))
+      (printf "\tassert.Equal(t,\n %s,\n %s)\n" expected (ast->go ast)))
     (printf "}\n")))
 
 (defn emit-test [package file tests]
@@ -60,42 +51,32 @@
 (defn constants []
   (->>
    [(test "Constants"
-          ["nil" nil]
-          [true true]
-          [false false]
-          [1 1]
-          [3.14 3.14]
-          [2 '(inc 1)]
-          [10 '(* 4 2.5)]
-          ["`foo`" "foo"]
-          ["'x'" \x]
-          (binding [*data-readers* *cljs-data-readers*]
-            ["map[string]interface{}{`foo`: `bar`}"
-             (read-string "#js {:foo \"bar\"}")])
-          (binding [*data-readers* *cljs-data-readers*]
-            ["[]interface{}{\"foo\", \"bar\"}"
-             (read-string "#js [\"foo\", \"bar\"])")])
-          ["&js.Date{Millis: 1408642409602}"
-           #inst "2014-08-21T17:33:29.602-00:00"]
-          ["&CljsCoreUUID{Uuid: `15c52219-a8fd-4771-87e2-42ee33b79bca`}"
-           #uuid "15c52219-a8fd-4771-87e2-42ee33b79bca"]
-          ["&js.RegExp{Pattern: `x`, Flags: ``}" #"x"]
-          ["&js.RegExp{Pattern: ``, Flags: ``}" #""]
-          ["&CljsCoreSymbol{Ns: nil, Name: `x`, Str: `x`, Hash: float64(-555367584), Meta: nil}"
-           ''x]
-          ["&CljsCoreSymbol{Ns: `user`, Name: `x`, Str: `user/x`, Hash: float64(-568535109), Meta: nil}"
-           ''user/x]
-          ["&CljsCoreKeyword{Ns: nil, Name: `x`, Fqn: `x`, Hash: float64(2099068185)}"
-           :x]
-          ["&CljsCoreKeyword{Ns: `user`, Name: `x`, Fqn: `user/x`, Hash: float64(2085900660)}"
-           :user/x])]
+          "nil" nil
+          true true
+          false false
+          1 1
+          3.14 3.14
+          2 '(inc 1)
+          10 '(* 4 2.5)
+          "`foo`" "foo"
+          "'x'" \x
+          "map[string]interface{}{`foo`: `bar`}" (read-string "#js {:foo \"bar\"}")
+          "[]interface{}{\"foo\", \"bar\"}" (read-string "#js [\"foo\", \"bar\"])")
+          "&js.Date{Millis: 1408642409602}" #inst "2014-08-21T17:33:29.602-00:00"
+          "&CljsCoreUUID{Uuid: `15c52219-a8fd-4771-87e2-42ee33b79bca`}" #uuid "15c52219-a8fd-4771-87e2-42ee33b79bca"
+          "&js.RegExp{Pattern: `x`, Flags: ``}" #"x"
+          "&js.RegExp{Pattern: ``, Flags: ``}" #""
+          "&CljsCoreSymbol{Ns: nil, Name: `x`, Str: `x`, Hash: float64(-555367584), Meta: nil}" ''x
+          "&CljsCoreSymbol{Ns: `user`, Name: `x`, Str: `user/x`, Hash: float64(-568535109), Meta: nil}" ''user/x
+          "&CljsCoreKeyword{Ns: nil, Name: `x`, Fqn: `x`, Hash: float64(2099068185)}" :x
+          "&CljsCoreKeyword{Ns: `user`, Name: `x`, Fqn: `user/x`, Hash: float64(2085900660)}" :user/x)]
    (emit-test "go_test" "constants_test")))
 
 (defn special-forms []
   (->>
    [(test "Let"
-          [1 '(let [y 1]
-                y)])
+          1 '(let [y 1]
+               y))
     ;; (test "Letfn"
     ;;          ["`bar`" '(letfn [(foo [] "bar")]
     ;;                      (foo))])
@@ -139,78 +120,83 @@
     ;; }()
 
     (test "If"
-          [true '(let [y :foo]
-                   (if y true false))]
-          [1 '(let [y true
-                    z (if y 1 0)]
-                z)])
+          true '(let [y :foo]
+                  (if y true false))
+          1 '(let [y true
+                   z (if y 1 0)]
+               z))
     (test "Loop"
-          [5 '(loop [y 0]
-                (if (== y 5)
-                  y
-                  (recur (inc y))))])
+          5 '(loop [y 0]
+               (if (== y 5)
+                 y
+                 (recur (inc y)))))
     (test "Do"
-          [3 '(do 1 2 3)])
-    (test-setup-toplevel
-     '[(def x 2)])
+          3 '(do 1 2 3))
+    (test-setup '[(def x 2)])
     (test "Def"
-          [2 'x])
+          2 'x)
     (test "New"
-          ["&js.Date{Millis: 0}" '(js/Date. 0)])
+          "&js.Date{Millis: 0}" '(js/Date. 0))
     (test "Dot"
-          [1970 '(.getUTCFullYear (js/Date. 0))])
+          1970 '(.getUTCFullYear (js/Date. 0)))
     (test "Var"
-          ["math.Inf(1)" 'js/Infinity])
+          "math.Inf(1)" 'js/Infinity)
     (test "Case"
-          [true '(let [x 2]
-                   (case x
-                     2 true
-                     1 false
-                     0))])
+          true '(let [x 2]
+                  (case x
+                    2 true
+                    1 false
+                    0)))
     (test "JS"
-          ["reflect.Float64" '(let [x 1]
-                                (js* "reflect.ValueOf(x).Kind()"))]
-          ["reflect.Int" '(let [v (js* "reflect.ValueOf(1)")]
-                            (-> v .Type .Kind))])
+          "reflect.Float64" '(let [x 1]
+                               (js* "reflect.ValueOf(x).Kind()"))
+          "reflect.Int" '(let [v (js* "reflect.ValueOf(1)")]
+                           (-> v .Type .Kind)))
     (test "Try"
-          ["&js.Error{`Foo`}" '(try
-                                 (throw (js/Error. "Foo"))
-                                 (catch js/Error e
-                                   e))]
-          ["`TypeError`" '(try
-                            (throw (js/TypeError. "Foo"))
-                            (catch js/Error _
-                              "Error")
-                            (catch js/TypeError _
-                              "TypeError"))]
-          ["`Bar`" '(try
-                      "Bar"
-                      (catch js/Error e
-                        e)
-                      (finally
-                        "Baz"))]
-          ["map[string]interface{}{`finally`: true}"
-           '(let [x (js* "map[string]interface{}{}")]
-              (try
-                x
-                (finally
-                  (js* "x[`finally`] = true"))))]
-          ["map[string]interface{}{`catch`: true, `finally`: true, `last`: `finally`}"
-           '(let [x (js* "map[string]interface{}{}")]
-              (try
-                (throw (js/Error. "Foo"))
-                (catch js/Error _
-                  (js* "x[`catch`] = true")
-                  (js* "x[`last`] = `catch`")
-                  x)
-                (finally
-                  (js* "x[`finally`] = true")
-                  (js* "x[`last`] = `finally`"))))])]
+          "&js.Error{`Foo`}" '(try
+                                (throw (js/Error. "Foo"))
+                                (catch js/Error e
+                                  e))
+          "`TypeError`" '(try
+                           (throw (js/TypeError. "Foo"))
+                           (catch js/Error _
+                             "Error")
+                           (catch js/TypeError _
+                             "TypeError"))
+          "`Bar`" '(try
+                     "Bar"
+                     (catch js/Error e
+                       e)
+                     (finally
+                       "Baz"))
+          "map[string]interface{}{`finally`: true}"
+          '(let [x (js* "map[string]interface{}{}")]
+             (try
+               x
+               (finally
+                 (js* "x[`finally`] = true"))))
+          "map[string]interface{}{`catch`: true, `finally`: true, `last`: `finally`}"
+          '(let [x (js* "map[string]interface{}{}")]
+             (try
+               (throw (js/Error. "Foo"))
+               (catch js/Error _
+                 (js* "x[`catch`] = true")
+                 (js* "x[`last`] = `catch`")
+                 x)
+               (finally
+                 (js* "x[`finally`] = true")
+                 (js* "x[`last`] = `finally`")))))]
    (emit-test "go_test" "special_forms_test")))
 
 (deftest go-all-tests
   (binding [cljs.analyzer/*cljs-file* (:file (meta #'go-test))
-            cljs.compiler/*go-line-numbers* true]
+            cljs.compiler/*go-line-numbers* true
+            *data-readers* cljs.tagged-literals/*cljs-data-readers*]
     (constants)
     (special-forms)
     (go-test "./...")))
+
+(defmethod print-method cljs.tagged_literals.JSValue
+  [^JSValue d ^Writer w]
+  (.write w "#js ")
+  (print-method (.val d) w))
