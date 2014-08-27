c685d6d3ee203452442ae7cd707703f0e36dde82
Håkan Råberg <hraberg@yahoo.se>
2014-08-27 20:21:03 +0100

    ClojureScript 0.0-2322

--- project.clj
+++ project.clj
@@ -5,6 +5,6 @@
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :main cljs.go
   :dependencies [[org.clojure/clojure "1.7.0-alpha1"]
-                 [org.clojure/clojurescript "0.0-2311"]
+                 [org.clojure/clojurescript "0.0-2322"]
                  [org.clojure/tools.analyzer "0.5.2"]
                  [org.clojure/core.async "0.1.319.0-6b1aca-alpha"]])
