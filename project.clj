0af62b84bc934869c899bf71942de6ccc6b7d17b
Håkan Råberg <hraberg@yahoo.se>
2014-08-30 02:35:13 +0100

    Latest core.async, not that we're close to this area yet.

--- project.clj
+++ project.clj
@@ -7,4 +7,4 @@
   :dependencies [[org.clojure/clojure "1.7.0-alpha1"]
                  [org.clojure/clojurescript "0.0-2322"]
                  [org.clojure/tools.analyzer.js "0.1.0-beta3"]
-                 [org.clojure/core.async "0.1.319.0-6b1aca-alpha"]])
+                 [org.clojure/core.async "0.1.338.0-5c5012-alpha"]])
