f06b0f353c6b23e59a022ae394f5a599f8251cca
Håkan Råberg <hraberg@yahoo.se>
2014-08-31 21:28:24 +0100

    Renaming marker fn

--- src/cljs/go/core.clj
+++ src/cljs/go/core.clj
@@ -794,7 +794,7 @@
   (warn-and-update-protocol p type env)
   (let [psym      (resolve p)]
     (cons
-     `(do (~'js* "func (_ *~{})~{}Marker() {}" ~type-sym ~psym))
+     `(do (~'js* "func (_ *~{})~{}__() {}" ~type-sym ~(symbol (cljs.compiler/go-type-fqn psym))))
      (if (= p 'Object)
        (add-obj-methods type type-sym sigs)
        (concat
@@ -1018,7 +1018,7 @@
                        (apply core/str)))]
     `(do
        (def ~psym) ;; Empty init gets dropped by the compiler, but registered by the analyzer.
-       (~'js* ~(core/str "type ~{} interface{\n" (core/str psym "Marker()\n")
+       (~'js* ~(core/str "type ~{} interface{\n" (core/str go-psym "__()\n")
                          (apply core/str (map method-decl methods)) "}\n")
               ~go-psym)
        (~'js* "func init() {\n\tRegisterProtocol_(~{}, (*~{})(nil))\n}"
