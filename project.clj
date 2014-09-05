b7b4575c657c30e2b0e7e11620819ba2bde9ee50
Håkan Råberg <hraberg@yahoo.se>
2014-09-06 00:17:11 +0100

    clojure 1.7.0-alpha2

--- project.clj
+++ project.clj
@@ -4,7 +4,7 @@
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :main cljs.go
-  :dependencies [[org.clojure/clojure "1.7.0-alpha1"]
+  :dependencies [[org.clojure/clojure "1.7.0-alpha2"]
                  [org.clojure/clojurescript "0.0-2322"]
                  [org.clojure/tools.analyzer.js "0.1.0-beta3"]
                  [org.clojure/core.async "0.1.338.0-5c5012-alpha"]])
