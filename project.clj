c5d17a68d032f4d2ab62146dde960c68383b8811
Håkan Råberg <hraberg@yahoo.se>
2014-10-27 18:22:23 +0000

    Updating deps.

--- project.clj
+++ project.clj
@@ -4,7 +4,7 @@
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :main cljs.go
-  :dependencies [[org.clojure/clojure "1.7.0-alpha2"]
+  :dependencies [[org.clojure/clojure "1.7.0-alpha3"]
                  [org.clojure/clojurescript "0.0-2371"]
                  [org.clojure/tools.analyzer.js "0.1.0-beta4"]
-                 [org.clojure/core.async "0.1.338.0-5c5012-alpha"]])
+                 [org.clojure/core.async "0.1.346.0-17112a-alpha"]])
