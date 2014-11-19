065c175c4738c3f4b25e76bfdebad82c32776ac2
Håkan Råberg <hraberg@yahoo.se>
2014-11-19 14:49:38 +0000

    Clojure 1.7.0-alpha4

--- project.clj
+++ project.clj
@@ -5,7 +5,7 @@
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :main cljs.go
   :aliases {"compile-clojurescript" ["run" "-m" "cljs.go/compile-clojurescript"]}
-  :dependencies [[org.clojure/clojure "1.7.0-alpha3"]
+  :dependencies [[org.clojure/clojure "1.7.0-alpha4"]
                  [org.clojure/clojurescript "0.0-2371"]
                  [org.clojure/tools.analyzer.js "0.1.0-beta4"]
                  [org.clojure/core.async "0.1.346.0-17112a-alpha"]])
